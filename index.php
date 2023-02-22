ca41a375476c603b3751c069f72be0843c138500
dani-fdez-g <119691872+dani-fdez-g@users.noreply.github.com>
2023-02-22 16:45:42 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,115 +1,25 @@
-<!DOCTYPE html PUBLIC>
-<html>
-<head>
-    <?php
-    include 'config/metas.php'
-    ?>
-    <title>HOME - RICOPOLLO</title>
-</head> 
+<?php
+include_once 'controller/productoControler.php';
+include_once 'controller/usuarioControler.php';
+include_once 'config/parametros.php'; 
 
-<body>
-    <header>
-        
-    <?php 
-    // se incluyen las paginas necesarias
-    require_once 'assets/includes/cabecera.php';
-    include_once 'cargarProductos.php';
-    
-    ?>
-    
-    </header>
-<body>
-     <!-- menu -->
-        <div id="carrusel" class="carousel slide mt-1" data-bs-ride="true">
-            <div class="carousel-inner">
-                <div class="carousel-item ">
-                    <a href="carta.php"><img src="assets/img/carusel 1.png" class="d-block w-100" alt="oferta del mes"></a>
-                </div>
-                <div class="carousel-item active">
-                <a href="carta.php"><img src="assets/img/carusel 2.png" class="d-block w-100" alt="oferta del mes"></a>
-                </div>
-                <div class="carousel-item">
-                    <<a href="carta.php"><img src="assets/img/carusel 3.png" class="d-block w-100" alt="oferta del mes"></a>
-                </div>
-            </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carrusel" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carrusel" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
-    <!-- fin menu -->
-    <section class="container-producto" > <!--para compartir-->
-           <h3 class="titulos ">PARA COMPARTIR</h3>
-                <?php 
-                //se recorre el array para mostrar los productos del mismo
-                foreach ($cubos as $key => $producto){ ?>
-                <div class="card">
-                    <div class="card-name-product">
-                        <p class="nombre-producto"><?php echo $producto->getNombre();?></p>
-                    </div>
-                    <div style="width:230px; height: 226px; background-image: URL(<?php echo $producto->getImagen();?>)" class="card-image-product">
-                        <div class="des-producto">
-                           <h3 class="titulo">INGREDIENTES</h3>
-                           <p class="descripcion"><?php echo $producto ->getDescripcion();?></p>
-                        </div>
-                    </div>
-                    <div class="card-body-producto">
-                        <p class="precio"><?php echo $producto->getPrecio();?>$</p>
-                        <form action="carta.php" method='post'>
+if(!isset($_GET['controller'])){
+    header("Location:$base_url"."?controller=producto" );
+} else{
+    $nombre_controller = $_GET['controller'].'Controller';
+    if(class_exists($nombre_controller)){
+        $controlador = new $nombre_controller;
+        if(isset($_GET['action'])){
+            $action = $_GET['action'];
+        }else{
+            $action = 'index';
+        }
+        $controlador -> $action();
+    }else{
+        header("Location:$base_url"."?controller=producto" );
+    }
+}
+unset($_GET['controller']);
+unset($_GET['action']);
+?>
 
-                                <input type="hidden" name="producto-cubo" value="<?php echo $key;?>">
-                                <button class="btn-add" type="submit">AÑADIR</button>
-                        </form>
-                    </div>
-                </div>
-            <?php }?>
-            <?php 
-            //se recorre el array para mostrar los productos del mismo
-            foreach ($tiras as $key => $producto){ ?>
-                <div class="card">
-                    <div class="card-name-product">
-                        <p class="nombre-producto"><?php echo $producto->getNombre();?></p>
-                    </div>
-                    <div style="width:230px; height: 226px; background-image: URL(<?php echo $producto->getImagen();?>)" class="card-image-product">
-                        <div class="des-producto">
-                           <h3 class="titulo">INGREDIENTES</h3>
-                           <p class="descripcion"><?php echo $producto ->getDescripcion();?></p>
-                        </div>
-                    </div>
-                    <div class="card-body-producto">
-                        <p class="precio"><?php echo $producto->getPrecio();?>$</p>
-                        <form action="carta.php" method='post'>
-
-                                <input type="hidden" name="producto-tira" value="<?php echo $key;?>">
-                                <button class="btn-add" type="submit">AÑADIR</button>
-                        </form>
-                    </div>
-                </div>
-            <?php }?>
-    </section> <!--fin para compartir-->
-
-                    
-    <section>
-        <div class="publicidad">
-            <img class="publi-1 mt-1" src="assets/img/publi1.png">
-            <img class="que-te-apetece" src="assets/img/que-te-apetece-hoy.png">
-
-                <a class= "btn-pedir" type="submit" href="carta.php">PEDIR AHORA <img class="icons" src="assets/img/moto.png"></a>
-            
-        </div>
-    </section>
-
-    
-    <footer>
-        <!-- se incluye el footer -->
-    <?php require_once 'assets/includes/footer.php';?>
-    </footer>
-</body>
-
-<script src="assets/js/bootstrap.bundle.min.js"></script>
-
-</html>
